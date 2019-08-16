c237c6484051850232ee6dbc1c71be377547df15
terrill11 <terrill.lim@gmail.com>
2019-08-16 00:46:12 -0400

    added more models

--- train_test.py
+++ train_test.py
@@ -9,7 +9,8 @@ def cleanup(train_df):
     extra_cols = ['payment_amount', 'payment_date', 'payment_status',
                   'balance_due', 'collection_status', 'compliance_detail']
     useless_cols = ['violation_street_name', 'mailing_address_str_name', 'country', 'violation_description',
-                    'inspector_name', 'violator_name', 'grafitti_status', 'non_us_str_code']
+                    'inspector_name', 'violator_name', 'grafitti_status', 'non_us_str_code', 'violation_street_number']
+
     train_df.drop(extra_cols, axis=1, inplace=True)
     train_df.drop(useless_cols, axis=1, inplace=True)
     train_df.dropna(subset=['compliance'], inplace=True)
@@ -32,38 +33,71 @@ def cleanup(train_df):
     #ticket_ids = train_df['ticket_id']
     train_df.drop(columns=['compliance', 'ticket_id'], inplace=True)
 
-    X_train, X_test, y_train, y_test = train_test_split(train_df, train_labels, random_state=0)
+    imp_feats = ['late_fee', 'disposition_Responsible by Admission', 'discount_amount', 'disposition_Responsible by Default',\
+                 'judgment_amount', 'fine_amount']
 
-    from sklearn.ensemble import RandomForestClassifier
-    rfc = RandomForestClassifier(random_state=0).fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(train_df.loc[:,imp_feats], train_labels, random_state=0)
 
-    features = dict(zip(X_train.columns, rfc.feature_importances_))
+
+    # from sklearn.ensemble import RandomForestClassifier
+    # rfc = RandomForestClassifier(n_estimators=10, max_features=9, random_state=0).fit(X_train, y_train)
+    # features = dict(zip(X_train.columns, rfc.feature_importances_))
     # return X_train, X_test, y_train, y_test
-    return sorted(features, key=features.get, reverse=True)[:9], X_train, X_test, y_train, y_test
+
+    return X_train, X_test, y_train, y_test
 
 
 def train_model():
-    keep_cols, X_train, X_test, y_train, y_test = cleanup(df)
-    #X_train, X_test, y_train, y_test = cleanup(df)
+    X_train, X_test, y_train, y_test = cleanup(df)
 
-    X_train = X_train.loc[:,keep_cols]
-    X_test = X_test.loc[:,keep_cols]
+    # from sklearn.preprocessing import MinMaxScaler
+    # scaler = MinMaxScaler()
+    # X_train = scaler.fit_transform(X_train)
+    # X_test = scaler.fit_transform(X_test)
+
 
     from sklearn.svm import LinearSVC
     l_svc = LinearSVC().fit(X_train, y_train)
     l_svc_predict = l_svc.predict(X_test)
     l_svc_score = roc_auc_score(y_test, l_svc_predict)
 
-
     from sklearn.linear_model import LogisticRegression
     lr = LogisticRegression().fit(X_train, y_train)
     lr_predict = lr.predict(X_test)
     lr_score = roc_auc_score(y_test, lr_predict)
 
-    #return l_svc_score
-    return l_svc_score, lr_score
+    from sklearn.ensemble import RandomForestClassifier
+    rfc = RandomForestClassifier(n_estimators=10, max_features=6, random_state=0).fit(X_train, y_train)
+    rfc_predict = rfc.predict(X_test)
+    rfc_score = roc_auc_score(y_test, rfc_predict)
 
+    from sklearn.neighbors import KNeighborsClassifier
+    knn = KNeighborsClassifier().fit(X_train, y_train)
+    knn_predict = knn.predict(X_test)
+    knn_score = roc_auc_score(y_test, knn_predict)
+
+
+    #return l_svc_score
+    return l_svc_score, lr_score, rfc_score, knn_score, sgd_score
+
+#print(cleanup(df))
 print(train_model())
 
 # LogisticRegression score: 0.547
 # LinearSVC score: 0.603
+# RandomForestClassifier score: 0.621
+
+
+# importan features?
+# ['late_fee', 'disposition_Responsible by Admission', 'discount_amount', 'disposition_Responsible by Default',\
+#     'judgment_amount', 'fine_amount', 'agency_name_Buildings, Safety Engineering & Env Department',\
+#     'agency_name_Detroit Police Department', 'agency_name_Department of Public Works', 'agency_name_Health Department',\
+#     'disposition_Responsible by Determination', 'agency_name_Neighborhood City Halls', 'admin_fee', 'state_fee',\
+#     'clean_up_cost', 'disposition_Responsible (Fine Waived) by Deter']
+
+
+# things to do:
+# - feature engineering
+# - min/max scaling with mean/median
+# - correlation
+# - Univariate Selection - https://towardsdatascience.com/feature-selection-techniques-in-machine-learning-with-python-f24e7da3f36e
