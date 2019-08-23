ad35bea9f3f1423ad30c935bc819e6079598d45a
terrill11 <terrill.lim@gmail.com>
2019-08-22 23:26:56 -0400

    final test model

--- train_test.py
+++ train_test.py
@@ -20,9 +20,9 @@ def state_cols(df):
     state_df['1_pc_total'] = state_df['1']/(state_df['0']+state_df['1'])
     state_df['count'] = state_df['0']+state_df['1']
 
-    final = state_df[((state_df['0_pc_total'] > 0.98) | (state_df['1_pc_total'] > 0.1)) & (state_df['count'] > 25)]
-
-    #return list(final['state'])
+    final = state_df[(state_df['1_pc_total'] > 0.15) & (state_df['count'] > 25)]
+    # return final
+    # return list(final['state'])
     return list(state_df[~state_df['state'].isin(list(final['state']))]['state'])
     
 #print(state_cols(df))
@@ -37,102 +37,68 @@ def cleanup(train_df):
     train_df.drop(useless_cols, axis=1, inplace=True)
     train_df.dropna(subset=['compliance'], inplace=True)
     
-    # Dropping location columns for the sake of simplicity.
-    # Dropping violation_code for now and get baseline results.
-    # May add it back into columns if needed.
-    train_df.drop(['ticket_issued_date', 'hearing_date', 'city',
-                   'zip_code', 'violation_code', 'state'], axis=1, inplace=True)
-                    #'state'
-    # Dropping remaining location columns.
-    # Added benefit of removing columns with high number of null values to simplify further
-    train_df.drop(['violation_zip_code', 'mailing_address_str_number'], axis=1, inplace=True)
-
-    # dropping more columns before dummies (remaining columns)
-    # ['ticket_id', 'agency_name', 'state', 'disposition', 'fine_amount', 'admin_fee', 'state_fee', 'late_fee', 'discount_amount',\
-    #  'clean_up_cost', 'judgment_amount', 'compliance', 'state']
-
-    #return train_df.corr()['compliance'].sort_values()
-    #train_df.drop(['fine_amount', 'judgment_amount', 'late_fee', 'state_fee', 'clean_up_cost', 'admin_fee'], axis=1, inplace=True)
-    #return train_df.columns
+    # more useless columns
+    more_useless_cols = ['ticket_issued_date', 'hearing_date', 'city', 'zip_code', 'violation_code', 'admin_fee',
+                         'state_fee', 'clean_up_cost', 'mailing_address_str_number', 'violation_code', 'violation_zip_code']
+    train_df.drop(more_useless_cols, axis=1, inplace=True)
 
     #getting dummies
     train_df = pd.get_dummies(train_df)
-    # unwanted_states = state_cols(df)
-    # train_df.drop(unwanted_states, axis=1, inplace=True)
 
-    #return train_df.corr()['compliance'].sort_values()
-    #test data
-    train_labels = train_df['compliance']
-    #ticket_ids = train_df['ticket_id']
-    train_df.drop(columns=['compliance', 'ticket_id'], inplace=True)
+    #add states in features
+    unwanted_states = state_cols(df)
+    train_df.drop(unwanted_states, axis=1, inplace=True)
 
-    imp_feats = ['disposition_Responsible by Default', 'late_fee', 'disposition_Responsible by Admission',\
-                 'disposition_Responsible by Determination', 'discount_amount', 'disposition_Responsible (Fine Waived) by Deter']
-    train_df.drop(columns=['admin_fee','state_fee','clean_up_cost'], inplace=True)
+    imp_feats = ['fine_amount', 'late_fee', 'discount_amount', 'judgment_amount', 'state_KS', 'state_NB', 'state_RI',\
+                'disposition_Responsible (Fine Waived) by Deter', 'disposition_Responsible by Admission',\
+                'disposition_Responsible by Determination']
     train_df.dropna(inplace=True)
 
+    train_labels = train_df['compliance']
     X_train, X_test, y_train, y_test = train_test_split(train_df[imp_feats], train_labels, random_state=0)
-    #X_train, X_test, y_train, y_test = train_test_split(train_df, train_labels, random_state=0)
 
-    #return X_train.corr()['compliance'].sort_values()
-    #return X_train.columns
-    
     return X_train, X_test, y_train, y_test
 
 
 def train_model():
     X_train, X_test, y_train, y_test = cleanup(df)
 
-    # from sklearn.preprocessing import MinMaxScaler
-    # scaler = MinMaxScaler()
-    # X_train = scaler.fit_transform(X_train)
-    # X_test = scaler.fit_transform(X_test)
-
-    from sklearn.linear_model import LogisticRegression
-    lr = LogisticRegression().fit(X_train, y_train)
-    lr_predict = lr.predict(X_test)
-    lr_score = roc_auc_score(y_test, lr_predict)
-
-    from sklearn.svm import LinearSVC
-    l_svc = LinearSVC().fit(X_train, y_train)
-    l_svc_predict = l_svc.predict(X_test)
-    l_svc_score = roc_auc_score(y_test, l_svc_predict)
+    #from sklearn.metrics import confusion_matrix
 
     from sklearn.tree import DecisionTreeClassifier
     dtc = DecisionTreeClassifier().fit(X_train, y_train)
     dtc_predict = dtc.predict(X_test)
     dtc_score = roc_auc_score(y_test, dtc_predict)
+    #print('DecisionTreeClassifier:\n', confusion_matrix(y_test, dtc_predict))
 
     from sklearn.ensemble import RandomForestClassifier
-    rfc = RandomForestClassifier(n_estimators=10, random_state=0).fit(X_train, y_train)
+    rfc = RandomForestClassifier(random_state=0).fit(X_train, y_train)
     rfc_predict = rfc.predict(X_test)
     rfc_score = roc_auc_score(y_test, rfc_predict)
+    #print('RandomForestClassifier:\n', confusion_matrix(y_test, rfc_predict))
 
     from sklearn.neighbors import KNeighborsClassifier
     knn = KNeighborsClassifier().fit(X_train, y_train)
     knn_predict = knn.predict(X_test)
     knn_score = roc_auc_score(y_test, knn_predict)
+    #print('KNeighborsClassfier:\n', confusion_matrix(y_test, knn_predict))
+
+    
+    return dtc_score, rfc_score, knn_score
 
 
-    #return l_svc_score
-    return lr_score, l_svc_score, dtc_score, rfc_score, knn_score
+def evaulate_targets(df):
+    final = df.groupby(['compliance']).std()
 
+    for i in final.columns:
+        print(i)
+        print(final[i])
+        print(final[i].loc[0]/final[i].loc[1] - 1)
+        print('\n')
+
+    pass
+
+#print(evaulate_targets(cleanup(df)))
 
 #print(cleanup(df))
 print(train_model())
-
-
-# importan features?
-# ['late_fee', 'disposition_Responsible by Admission', 'discount_amount', 'disposition_Responsible by Default',\
-#     'judgment_amount', 'fine_amount', 'agency_name_Buildings, Safety Engineering & Env Department',\
-#     'agency_name_Detroit Police Department', 'agency_name_Department of Public Works', 'agency_name_Health Department',\
-#     'disposition_Responsible by Determination', 'agency_name_Neighborhood City Halls', 'admin_fee', 'state_fee',\
-#     'clean_up_cost', 'disposition_Responsible (Fine Waived) by Deter']
-# drop admin_fee, state_fee, clean_up_cost
-
-
-# things to do:
-# - feature engineering
-# - min/max scaling with mean/median
-# - correlation
-# - Univariate Selection - https://towardsdatascience.com/feature-selection-techniques-in-machine-learning-with-python-f24e7da3f36e
